a4424a0d2843e41c59c2bbf2fb9783ef649cda0b
wenston <suwencong2016@gmail.com>
2021-06-04 18:32:30 +0800

    facebook screenshot test code

--- app/src/androidTest/java/com/wencong/mvpcode/FBSnapShotTest.kt
+++ app/src/androidTest/java/com/wencong/mvpcode/FBSnapShotTest.kt
@@ -1,42 +1,25 @@
+@file:Suppress("DEPRECATION")
+
 package com.wencong.mvpcode
 
-import android.Manifest
-import android.view.LayoutInflater
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
-import com.facebook.testing.screenshot.Screenshot
-import com.facebook.testing.screenshot.ViewHelpers
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
+class FBSnapShotTest : SnapshotTest {
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-@LargeTest
-class FBSnapShotTest {
-
-    @Rule
-    var mRuntimePermissionRule: GrantPermissionRule =
-        GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+    @get:Rule
+    var permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     @Test
-    fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.wencong.mvpcodetest", appContext.packageName)
-
-        val view = LayoutInflater.from(appContext).inflate(R.layout.item_data, null, false)
-        ViewHelpers.setupView(view)
-            .setExactWidthDp(300)
-            .layout()
-        Screenshot.snap(view)
-            .record()
+    fun snapShotTest() {
+        val activity = activityRule.launchActivity(null)
+        compareScreenshot(activity)
     }
 }
