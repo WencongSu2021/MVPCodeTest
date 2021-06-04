26aad5c3302750376c3d7ff6e57e0565312e750d
wenston <suwencong2016@gmail.com>
2021-06-04 17:01:10 +0800

    facebook screenshot test

--- app/src/androidTest/java/com/wencong/mvpcode/FBSnapShotTest.kt
+++ app/src/androidTest/java/com/wencong/mvpcode/FBSnapShotTest.kt
@@ -1,11 +1,15 @@
 package com.wencong.mvpcode
 
+import android.Manifest
 import android.view.LayoutInflater
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
 import com.facebook.testing.screenshot.Screenshot
 import com.facebook.testing.screenshot.ViewHelpers
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,21 +20,23 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class FBSnapShotTest{
+@LargeTest
+class FBSnapShotTest {
+
+    @Rule
+    var mRuntimePermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
 
     @Test
     fun useAppContext() {
-        InstrumentationRegistry.getInstrumentation().runOnMainSync {
-            // Context of the app under test.
-            val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-            assertEquals("com.wencong.mvpcodetest", appContext.packageName)
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.wencong.mvpcodetest", appContext.packageName)
 
-            val view = LayoutInflater.from(appContext).inflate(R.layout.item_data, null, false)
-            ViewHelpers.setupView(view)
-                .setExactWidthDp(300)
-                .layout()
-            Screenshot.snap(view)
-                .record()
-        }
+        val view = LayoutInflater.from(appContext).inflate(R.layout.item_data, null, false)
+        ViewHelpers.setupView(view)
+            .setExactWidthDp(300)
+            .layout()
+        Screenshot.snap(view)
+            .record()
     }
 }
